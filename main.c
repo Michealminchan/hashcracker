e39b15f458d593a63835bbb615dbf8c262feffcb
conmarap <conmarap@gmail.com>
2017-10-02 21:57:36 -0400

    Add double hashing and group to_hex

--- main.c
+++ main.c
@@ -20,6 +20,7 @@ pthread_mutex_t lock;
 
 static int verbose_flag = 0;
 static int newlines_flag = 0;
+static int double_hash_flag = 0;
 static char *format;
 
 FILE *blob;
@@ -36,54 +37,63 @@ static struct option long_options[] = {
     { "wordlist", required_argument, 0,              'w' },
     { "target",   required_argument, 0,              't' },
     { "newlines", no_argument,       0,              'n' },
+    { "double",   no_argument,       0,              'd' },
     { "help",     no_argument,       0,              'h' },
     {  0,         0,                 0,               0  }
 };
 
 /**
+ * Convert the hash to hex and put it in the buffer.
+ *
+ * @param hash The hash to convert to hex.
+ * @param output_buffer The output buffer.
+ * @param length The length of the output buffer.
+ */
+void hash_to_hex(unsigned char *hash, char *output_buffer, int length)
+{
+    int i = 0;
+
+    for(i = 0; i < length; i++) {
+        sprintf(output_buffer + (i * 2), "%02x", hash[i]);
+    }
+
+    output_buffer[length * 2] = 0;
+}
+
+/**
  * Create an SHA256 hash.
  *
  * @param string The string to hash.
- * @param argv The output buffer.
+ * @param buffer The output buffer.
  */
-void sha256(char *string, char outputBuffer[65])
+void sha256(char *string, char output_buffer[65])
 {
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
     SHA256_Update(&sha256, string, strlen(string));
     SHA256_Final(hash, &sha256);
-    int i = 0;
 
-    for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
-    {
-        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
-    }
-
-    outputBuffer[64] = 0;
+    // Produce the hex 
+    hash_to_hex(hash, output_buffer, SHA256_DIGEST_LENGTH);
 }
 
 /**
  * Create an MD5 hash.
  *
  * @param string The string to hash.
- * @param argv The output buffer.
+ * @param buffer The output buffer.
  */
-void md5(char *string, char outputBuffer[33])
+void md5(char *string, char output_buffer[33])
 {
     unsigned char hash[MD5_DIGEST_LENGTH];
     MD5_CTX md5;
     MD5_Init(&md5);
     MD5_Update(&md5, string, strlen(string));
     MD5_Final(hash, &md5);
-    int i = 0;
 
-    for(i = 0; i < MD5_DIGEST_LENGTH; i++)
-    {
-        sprintf(outputBuffer + (i * 2), "%02x", hash[i]);
-    }
-
-    outputBuffer[33] = 0;
+    // Produce the hex 
+    hash_to_hex(hash, output_buffer, MD5_DIGEST_LENGTH);
 }
 
 /**
@@ -136,6 +146,11 @@ void *worker_callback(void *ptr)
         char hash[strcmp(format, "sha256") == 0 ? 65 : 33];
         hash_fun_ptr(msg_line, hash);
 
+        // Re-run the hash function on, but on the hash itself this time.
+        if (double_hash_flag) {
+            hash_fun_ptr(hash, hash);
+        }
+
         if (verbose_flag != 0) {
             printf("%s -> %s%c", hash, msg_line, newlines_flag == 0 ? '\n' : '\0');
         }
@@ -160,6 +175,10 @@ void *worker_callback(void *ptr)
             if (blob) {
                 fclose(blob);
             }
+
+            exit(0);
+
+            // The thing below causes an error when run. Figure dat shit out.
 
             // Signal the other threads that we're donezo.
             pthread_mutex_lock(&lock);
@@ -191,6 +210,7 @@ void print_help()
     printf("         --format,   -f [hash_fun]: The hash function (md5|sha256)\n");
     printf("         --target,   -t [hash]:     The hash to check\n");
     printf("         --newlines, -n:            Whether to hash with newlines\n");
+    printf("         --double,   -d:            Double hash\n");
     printf("         --help,     -h:            Prints this message\n\n");
 }
 
@@ -231,12 +251,15 @@ void process_args(int c, int option_index)
         case 'n':
             newlines_flag = 1;
             break;
+        case 'd':
+            double_hash_flag = 1;
+            break;
         case 'v':
             verbose_flag = 1;
             break;
         case 'h':
             print_help();
-            break;
+            exit(1);
     }
 }
 
@@ -260,7 +283,7 @@ int main(int argc, char **argv)
     pthread_mutex_init(&lock, NULL);
     int c;
 
-    while ((c = getopt_long(argc, argv, "f:w:t:nhv", long_options, &option_index)) != -1) {
+    while ((c = getopt_long(argc, argv, "f:w:t:ndhv", long_options, &option_index)) != -1) {
         process_args(c, option_index);
     }
 
@@ -270,10 +293,14 @@ int main(int argc, char **argv)
         return 1;
     }
 
-    printf("Reading %s and producing %s", wordlist, format);
+    printf("Reading %s and producing %s\n", wordlist, format);
 
     if (newlines_flag != 0) {
-        printf(" with newlines");
+        printf("With newlines");
+    }
+
+    if (double_hash_flag != 0) {
+        printf(" and double hashing.");
     }
 
     printf("\n");
