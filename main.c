e34a4e4c5ccf164baf47bc243ea37b5185037654
Constantine Apostolou <conmarap@gmail.com>
2017-11-08 19:00:46 -0500

    Add more hashes and separate from main

--- main.c
+++ main.c
@@ -14,6 +14,7 @@
 #include <pthread.h>
 #include <openssl/sha.h>
 #include <openssl/md5.h>
+#include "hash.h"
 
 clock_t begin;
 pthread_mutex_t lock;
@@ -30,6 +31,7 @@ int count = 0;
 void (*hash_fun_ptr)(char *, char *);
 int found = 0;
 int applied_hashing = 1;
+int hash_length = 0;
 
 static struct option long_options[] = {
     { "verbose",  no_argument,       0,               1  },
@@ -41,60 +43,6 @@ static struct option long_options[] = {
     { "help",     no_argument,       0,              'h' },
     {  0,         0,                 0,               0  }
 };
-
-/**
- * Convert the hash to hex and put it in the buffer.
- *
- * @param hash The hash to convert to hex.
- * @param output_buffer The output buffer.
- * @param length The length of the output buffer.
- */
-void hash_to_hex(unsigned char *hash, char *output_buffer, int length)
-{
-    int i = 0;
-
-    for(i = 0; i < length; i++) {
-        sprintf(output_buffer + (i * 2), "%02x", hash[i]);
-    }
-
-    output_buffer[length * 2] = 0;
-}
-
-/**
- * Create an SHA256 hash.
- *
- * @param string The string to hash.
- * @param buffer The output buffer.
- */
-void sha256(char *string, char output_buffer[65])
-{
-    unsigned char hash[SHA256_DIGEST_LENGTH];
-    SHA256_CTX sha256;
-    SHA256_Init(&sha256);
-    SHA256_Update(&sha256, string, strlen(string));
-    SHA256_Final(hash, &sha256);
-
-    // Produce the hex 
-    hash_to_hex(hash, output_buffer, SHA256_DIGEST_LENGTH);
-}
-
-/**
- * Create an MD5 hash.
- *
- * @param string The string to hash.
- * @param buffer The output buffer.
- */
-void md5(char *string, char output_buffer[33])
-{
-    unsigned char hash[MD5_DIGEST_LENGTH];
-    MD5_CTX md5;
-    MD5_Init(&md5);
-    MD5_Update(&md5, string, strlen(string));
-    MD5_Final(hash, &md5);
-
-    // Produce the hex 
-    hash_to_hex(hash, output_buffer, MD5_DIGEST_LENGTH);
-}
 
 /**
  * Returns the size of the blob.
@@ -143,7 +91,7 @@ void *worker_callback(void *ptr)
         }
 
         // Allocate the appropriate memory for the 
-        char hash[strcmp(format, "sha256") == 0 ? 65 : 33];
+        char hash[hash_length];
         hash_fun_ptr(msg_line, hash);
 
         // Re-run the hash function on, but on the hash itself this time.
@@ -209,7 +157,7 @@ void print_help()
     printf("hashchcker\n\n");
     printf("Usage:   hashchcker [options]\n\n");
     printf("Options: --wordlist, -w [filename]: The wordlist to check against\n");
-    printf("         --format,   -f [hash_fun]: The hash function (md5|sha256)\n");
+    printf("         --format,   -f [hash_fun]: The hash function (md5|sha|sha1|sha256|sha512)\n");
     printf("         --target,   -t [hash]:     The hash to check\n");
     printf("         --newlines, -n:            Whether to hash with newlines\n");
     printf("         --amount,   -a:            Amount of hashing\n");
@@ -230,11 +178,21 @@ void process_args(int c, int option_index)
         case 'f':
             format = optarg;
 
-            if (strcmp(format, "sha256") == 0) {
+            if (strcmp(format, "sha512") == 0) {
+                hash_fun_ptr = &sha512;
+                hash_length = 128;
+            }
+            else if (strcmp(format, "sha256") == 0) {
                 hash_fun_ptr = &sha256;
+                hash_length = 65;
+            }
+            else if (strcmp(format, "sha") == 0 || strcmp(format, "sha1") == 0) {
+                hash_fun_ptr = strcmp(format, "sha") == 0 ? &sha : &sha1;
+                hash_length = 40;
             }
             else if (strcmp(format, "md5") == 0) {
                 hash_fun_ptr = &md5;
+                hash_length = 33;
             }
             else {
                 printf("Unknown format %s\n", format);
